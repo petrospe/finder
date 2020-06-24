72b43b55d0041ba5b38539f41e58233144e96a08
petrospe <petros_pe@yahoo.gr>
2020-06-24 22:22:07 +0300

    vue auth implementation

--- resources/js/auth.js
+++ resources/js/auth.js
@@ -14,4 +14,5 @@ const config = {
   logoutData: {url: 'auth/logout', method: 'POST', redirect: '/', makeRequest: true},
   fetchData: {url: 'auth/user', method: 'GET', enabled: true},
   refreshData: {url: 'auth/refresh', method: 'GET', enabled: true, interval: 30}
-}export default config
+}
+export default config
